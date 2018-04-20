d38947ab06b52c31cb3c08ebada161704f62e8f7
XueHuaDaShen <xyh_gzyx@163.com>
2018-04-20 18:35:11 +0800

    2018-4-20 解决了静态 js 文件的引入问题，创建dialog弹出框组件，以及诸多小组件

---
+++ lottery-cms/project/src/components/littleStyle/tableBtn.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="display:inline">
+        <el-button style="margin-left:.5rem" plain size="small" @click="func" round type="info">{{text}}</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'tableBtn',
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        func: {
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
+
